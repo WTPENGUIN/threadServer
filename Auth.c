fcc577daf9b22f84e78ff9cfe0ce99e4f2e8cb01
WTPENGUIN <ccho2932@naver.com>
2020-10-22 10:58:46 +0900

    인덴트 수정

--- Auth.c
+++ Auth.c
@@ -4,53 +4,51 @@
 #endif
 
 int auth(Login USER) {
-
-	MYSQL       *connection=NULL, conn;
+    
+    MYSQL       *connection=NULL, conn;
     MYSQL_RES   *sql_result;
     MYSQL_ROW   sql_row;
     int         query_stat;
-	char        table_name[5] = "USER"; 
+    char        table_name[5] = "USER"; 
 
     char ID[10];
     char PASSWD[16];
-	char QUERY[100];
-	int result;
-    
+    char QUERY[100];
+    int result;
+
     mysql_init(&conn);
 
     connection = mysql_real_connect(&conn, DB_HOST,
                                     DB_USER, DB_PASS,
                                     DB_NAME, DB_PORT,
                                     (char *)NULL, 0);
-
-    if (connection == NULL)
-    {
+                                    
+    if (connection == NULL) {
         fprintf(stderr, "Mysql connection error : %s", mysql_error(&conn));
         return 1;
     }
 
 	sprintf(QUERY, "select PASSWD from %s where ID=\"%s\"", table_name, USER.ID);
-
     query_stat = mysql_query(connection, "select * from USER");
-    if (query_stat != 0)
-    {
+    
+    if (query_stat != 0) {
         fprintf(stderr, "Mysql query error : %s", mysql_error(&conn));
-
+        
         mysql_free_result(sql_result);
         mysql_close(connection);
         return -1;
     }
     
-	// Store result
+    // Store result
     sql_result = mysql_store_result(connection);
-	sql_row = mysql_fetch_row(sql_result);
+    sql_row = mysql_fetch_row(sql_result);
 
-	//Auth Begin
+    //Auth Begin
     #ifdef __DEBUG_
     printf("Auth : %s %s Check\n", sql_row[0], USER.PW);
     #endif
 
-	if(strcmp(sql_row[1], USER.PW) == 0) {
+    if(strcmp(sql_row[1], USER.PW) == 0) {
         #ifdef __DEBUG_
         printf("Auth Clear.\n");
         #endif
@@ -60,14 +58,12 @@ int auth(Login USER) {
         #ifdef __DEBUG_
         printf("Auth Fail.\n");
         #endif
-		result = -1;
-	}
+        result = -1;
+    }
 
-	//Auth End
+    //Auth End
 
     mysql_free_result(sql_result);
-
     mysql_close(connection);
-
-	return result;
+    return result;
 }
