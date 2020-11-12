1c0149154def544a257dcb23d03151f6c283c3ad
WTPENGUIN <ccho2932@naver.com>
2020-11-12 15:45:15 +0900

    Config 관련 코드 추가

--- Server_Init.c
+++ Server_Init.c
@@ -3,50 +3,45 @@
 #include "Server_Header.h"
 #endif
 
-// Server Init
 int server_Init(char *port) {
-
+	
 	int sv_Sock;
 	struct sockaddr_in sv;
 	int reuseADDR = 1;
+	
+	printf("서버 Config 읽는 중입니다....\n");
+	read_Config();
 
+	printf("초기화 완료, 서버 소켓 생성중입니다....\n");
 	sv_Sock = socket(PF_INET, SOCK_STREAM, 0);
-
+	
 	if(sv_Sock < 0) {
-		#ifdef __DEBUG_
 		perror("socket fail.");
-		#endif
 		exit(1);
 	}
-
+	
 	// Set Socket REUSEADDR Option
 	if(setsockopt(sv_Sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuseADDR, sizeof(reuseADDR)) < 0) {
-		#ifdef __DEBUG_
 		perror("SERVER Setting Socket Option fail.");
-		#endif
 		exit(1);
 	}
-
+	
 	memset(&sv, 0, sizeof(sv));
 	sv.sin_family = AF_INET;
 	sv.sin_addr.s_addr = htonl(INADDR_ANY);
 	sv.sin_port = htons(atoi(port));
-
+	
 	// Bind Fail.
 	if(bind(sv_Sock, (struct sockaddr*) &sv, sizeof(sv)) < 0) {
-		#ifdef __DEBUG_
 		perror("bind error");
-		#endif
 		exit(1);
 	}
-
+	
 	// Listen Fail.
 	if(listen(sv_Sock, 5) < 0) {
-		#ifdef __DEBUG_
 		perror("listen error");
-		#endif
 		exit(1);
 	}
-
+	
 	return sv_Sock;
 }
