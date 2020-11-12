5ee03eea7497b8ac47df1878b67b328c2435a10f
WTPENGUIN <ccho2932@naver.com>
2020-11-12 15:43:26 +0900

    DB 동작 방식 변경

--- Auth.c
+++ Auth.c
@@ -5,38 +5,36 @@
 
 int auth(Login USER) {
     
-    MYSQL *connection=NULL, conn;
+    MYSQL *connection = NULL, conn;
     MYSQL_RES *sql_result;
     MYSQL_ROW sql_row;
     int query_stat;
     char db_name[8] = "ACCOUNT";
     char table_name[5] = "USER"; 
-
+    
     char ID[10];
     char PASSWD[16];
     char QUERY[100];
     int result;
-
+    
     mysql_init(&conn);
-
-    connection = mysql_real_connect(&conn, DB_HOST,
-                                    DB_USER, DB_PASS,
-                                    db_name, DB_PORT,
-                                    (char *)NULL, 0);
-                                    
+    
+    connection = mysql_real_connect(&conn, cfg.HOST, cfg.USER, cfg.PASS, db_name, cfg.PORT, (char *)NULL, 0);
+    
     if (connection == NULL) {
         fprintf(stderr, "Mysql connection error : %s", mysql_error(&conn));
         return 1;
     }
-
-	sprintf(QUERY, "select PASSWD from %s where ID=\"%s\"", table_name, USER.ID);
+    
+    sprintf(QUERY, "select PASSWD from %s where ID=\"%s\"", table_name, USER.ID);
     query_stat = mysql_query(connection, QUERY);
+    
     #ifdef __DEBUG_
     printf("%s\n",QUERY);
     #endif
     
     if (query_stat != 0) {
-        fprintf(stderr, "Mysql query error : %s", mysql_error(&conn));
+        fprintf(stderr, "Mysql query error : %s\n", mysql_error(&conn));
         
         mysql_free_result(sql_result);
         mysql_close(connection);
@@ -46,27 +44,28 @@ int auth(Login USER) {
     // Store result
     sql_result = mysql_store_result(connection);
     sql_row = mysql_fetch_row(sql_result);
-
+    
     //Auth Begin
     #ifdef __DEBUG_
     printf("Auth : %s %s Check\n", sql_row[0], USER.PW);
     #endif
-
+    
     if(!strcmp(sql_row[0], USER.PW)) {
         #ifdef __DEBUG_
         printf("Auth Clear.\n");
         #endif
+        
         result = 1;
     }
     else {
         #ifdef __DEBUG_
         printf("Auth Fail.\n");
         #endif
+        
         result = -1;
     }
-
     //Auth End
-
+    
     mysql_free_result(sql_result);
     mysql_close(connection);
     return result;
