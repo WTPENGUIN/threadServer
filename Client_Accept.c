31ebee845fa895df7ac01f958bd62ccde1f29a00
WTPENGUIN <ccho2932@naver.com>
2020-11-13 15:02:48 +0900

    주석 추가, 디버그 모드 메세지 출력

--- Client_Accept.c
+++ Client_Accept.c
@@ -11,82 +11,95 @@ const char LOG_SUC_DEBUG[]  = "LOGIN_SUCCESS_DEBUG\n";
 
 // Client Connection accept *FORM( ID:PASSWD:LOGINMODE )
 Client cl_Accept(int sv_Sock) {
+
+	struct sockaddr_in cl;       // Client Socket Struct
 	int cl_Sock;                 // Client Socket Distritor
-	int cl_size;                 // Client Struct Size
+	int cl_size = sizeof(cl);    // Client Struct Size
+	
 	int read_b;                  // Read bytes
 	char read_msg[BUF_SIZE];     // Read Message
-	
-	struct sockaddr_in cl;       // Client Socket Struct 
-	Client __currntClient;       // Client Structure for login success
-	Login __currntLogin;         // LOGIN Structure
+
+	Client __currentClient;       // Client Structure for login success
+	Login __currentLogin;         // LOGIN Structure
 	
 	// init Struct
-	memset(&__currntClient, 0, sizeof(struct Client));
-	memset(&__currntLogin, 0, sizeof(struct Login));
-	
-	cl_size = sizeof(cl);
+	memset(&__currentClient, 0, sizeof(struct Client));
+	memset(&__currentLogin, 0, sizeof(struct Login));
+
+	// Accept connection 
 	cl_Sock = accept(sv_Sock, (struct sockaddr*)&cl, &cl_size);
 	
-	// accept fail
+	// If accept fail, return __currentClient(Socket Number : -1)
 	if(cl_Sock < 0) {
 		#ifdef __DEBUG_
 		perror("accept fail.");
 		#endif
 		
-		__currntClient.num_sock = -1;
-		return __currntClient;
+		__currentClient.num_sock = -1;
+		return __currentClient;
 	}
 	
+	// Read login message 
 	read_b = read(cl_Sock, &read_msg, BUF_SIZE);
-	
-	// unused data cut
 	read_msg[read_b] = '\0';
 	#ifdef __DEBUG_
 	printf("Read Complete. %s\n", read_msg);
 	#endif
 	
-	// Read Fail Login Structure
+	// If read login message, return __currentClient(Socket Number : -1)
 	if(read_b <= 0 ) {
 		#ifdef __DEBUG_
 		printf("Read Login Info faild\n");
 		#endif
 		
+		// Write error message to client socket
 		write(cl_Sock, LOG_ERR_F, sizeof(LOG_ERR_F));
-		__currntClient.num_sock = -1;
+
+		__currentClient.num_sock = -1;
 		close(cl_Sock);
-		return __currntClient;
+		return __currentClient;
 	}
 	
 	#ifdef __DEBUG_
 	printf("Read Check Complete.\n");
 	#endif
 	
-	returnLogin(&__currntLogin, read_msg);
+	// Tokenization login message
+	tokenLogin(&__currentLogin, read_msg);
 	
-	//returnLogin Fail.
-	if(strcmp(__currntLogin.ID, "FAIL") == 0 && strcmp(__currntLogin.PW, "FAIL") == 0) {
+	// If tokenization login message , return __currentClient(Socket Number : -1)
+	if(strcmp(__currentLogin.ID, "FAIL") == 0 && strcmp(__currentLogin.PW, "FAIL") == 0) {
 		
 		#ifdef __DEBUG_
 		printf("Login Structure corrupted.\n");
 		#endif
 		
+		// Write error message to client socket
 		write(cl_Sock, LOG_ERR_F, sizeof(LOG_ERR_F));
-		__currntClient.num_sock = -1;
+
+		__currentClient.num_sock = -1;
 		close(cl_Sock);
-		return __currntClient;
+		return __currentClient;
 	}
 	
 	// Check Same connection
 	pthread_mutex_lock(&mutex_c);
 	for(int i = 0; i <= cl_num; i++) {
-		if(cl_socks[i].ID == __currntLogin.ID) {
+		if(cl_socks[i].ID == __currentLogin.ID) {
 			
-			//Same Connection Detect
+			// Same Connection Detect, return __currentClient(Socket Number : -1)
+			#ifdef __DEBUG_
+			printf("ID : %s is already Login!\n", __currentLogin.ID);
+			#endif
+
+			// Write error message to client socket
 			write(cl_Sock, LOG_ERR_A, sizeof(LOG_ERR_A));
-			__currntClient.num_sock = -1;
+
+			__currentClient.num_sock = -1;
 			close(cl_Sock);
+			// Need to unlock mutex in detect same connection situation
 			pthread_mutex_unlock(&mutex_c);
-			return __currntClient;
+			return __currentClient;
 		}
 	}
 	pthread_mutex_unlock(&mutex_c);
@@ -96,67 +109,74 @@ Client cl_Accept(int sv_Sock) {
 	#endif
 	
 	// Login Mode Check
-	if(__currntLogin.login_MODE < 0 || __currntLogin.login_MODE > 2 ) {
+	if(__currentLogin.login_MODE < 0 || __currentLogin.login_MODE > 2 ) {
 		
+		// Detect wrong login mode, return __currentClient(Socket Number : -1)
 		#ifdef __DEBUG_
-		printf("ID : %s is Wrong Login Mode!\n", __currntLogin.ID);
+		printf("ID : %s is Wrong Login Mode!\n", __currentLogin.ID);
 		#endif
 		
+		// Write error message to client socket
 		write(cl_Sock, LOG_ERR_F, sizeof(LOG_ERR_F));
-		__currntClient.num_sock = -1;
+		__currentClient.num_sock = -1;
 		close(cl_Sock);
-		return __currntClient;
+		return __currentClient;
 	}
 	
-	// Debug Mode Login
-	if(__currntLogin.login_MODE == 2) { 
+	// Debug Mode Login Process
+	if(__currentLogin.login_MODE == 2) { 
 		
 		#ifdef __DEBUG_ 
-		printf("ID : %s in DEBUG MODE!\n", __currntLogin.ID);
+		printf("ID : %s in DEBUG MODE!\n", __currentLogin.ID);
 		#endif
 		
+		// Write Login clear message
 		write(cl_Sock, LOG_SUC_DEBUG, sizeof(LOG_SUC_DEBUG));
 		
-		// Client Structure setting start
-		strncpy(__currntClient.ID, __currntLogin.ID, strlen(__currntLogin.ID)); // ID SET
-		__currntClient.num_sock = cl_Sock;                                      // Socket Number Set
-		memcpy(&(__currntClient.info_client), &cl, sizeof(struct sockaddr_in)); // Socket Structure Set
-		__currntClient.login_MODE = __currntLogin.login_MODE;                   // Login Mode Set
+		// Client structure set
+		strncpy(__currentClient.ID, __currentLogin.ID, strlen(__currentLogin.ID));   // ID SET
+		__currentClient.num_sock = cl_Sock;                                          // Socket Number Set
+		memcpy(&(__currentClient.info_client), &cl, sizeof(struct sockaddr_in));     // Socket Structure Set
+		__currentClient.login_MODE = __currentLogin.login_MODE;                      // Login Mode Set
 		
 		#ifdef __DEBUG_
-		printf("ID(client_debug) : %s Check.\n", __currntClient.ID);
-		printf("ID(login_debug) : %s Check.\n", __currntLogin.ID);
+		printf("ID(client_debug) : %s Check.\n", __currentClient.ID);
+		printf("ID(login_debug) : %s Check.\n", __currentLogin.ID);
 		#endif
 		
-		return __currntClient;
+		// retrun __currentClient(Socket Number : connected socket number)
+		return __currentClient;
 	}
 	
-	// Auth Check
-	if(auth(__currntLogin) < 0) {
+	// Auth Check start
+	if(auth(__currentLogin) < 0) {
 		
+		// If Auth fail, return __currentClient(Socket Number : -1)
 		#ifdef __DEBUG_
-		printf("ID : %s Auth Fail.\n", __currntLogin.ID);
+		printf("ID : %s Auth Fail.\n", __currentLogin.ID);
 		#endif
 		
+		// Write error message to client socket
 		write(cl_Sock, LOG_ERR_F, sizeof(LOG_ERR_F));
-		__currntClient.num_sock = -1;
+
+		__currentClient.num_sock = -1;
 		close(cl_Sock);
-		return __currntClient;
+		return __currentClient;
 	}
 	
-	// Client Structure Set.
-	strncpy(__currntClient.ID, __currntLogin.ID, strlen(__currntLogin.ID));
-	__currntClient.num_sock = cl_Sock;
-	memcpy(&(__currntClient.info_client), &cl, sizeof(struct sockaddr_in));
-	__currntClient.login_MODE = __currntLogin.login_MODE;
+	// Client structure set
+	strncpy(__currentClient.ID, __currentLogin.ID, strlen(__currentLogin.ID));
+	__currentClient.num_sock = cl_Sock;
+	memcpy(&(__currentClient.info_client), &cl, sizeof(struct sockaddr_in));
+	__currentClient.login_MODE = __currentLogin.login_MODE;
 	
 	#ifdef __DEBUG_
-	printf("ID(client) : %s Check.\n", __currntClient.ID);
-	printf("ID(login) : %s Check.\n", __currntLogin.ID);
+	printf("ID(client) : %s Check.\n", __currentClient.ID);
+	printf("ID(login) : %s Check.\n", __currentLogin.ID);
 	#endif
 	
-	//LOGIN MESSAGE
+	// Write Login clear message to client socket
 	write(cl_Sock, LOG_SUC, sizeof(LOG_SUC));
 	
-	return __currntClient;
+	return __currentClient;
 }
