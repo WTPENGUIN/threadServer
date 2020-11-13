ca368368cce96fc1fd5f38cb1de7ccd891421d15
WTPENGUIN <ccho2932@naver.com>
2020-11-13 15:05:29 +0900

    주석 추가, 동작 방식 변경

--- Config.c
+++ Config.c
@@ -3,49 +3,66 @@
 #include "Server_Header.h"
 #endif
 
+// Read JSON file and set server config
 void read_Config(void) {
     
     struct json_object_iterator it, itEnd;
     struct json_object* obj;
+    struct json_object* cur;
+
     const char* type;
     const char* name;
     
-    /* JSON type의 데이터를 읽는다. */
+    // Read JSON file, and make JSON object
     obj =  json_object_from_file("./config.json");
 
+    // If read JSON file fail, output error message and exit program
     if(obj == NULL) {
         printf("Check json file\n");
         exit(1);
     }
     
+    // Make JSON iterator
     it = json_object_iter_begin(obj);
     itEnd = json_object_iter_end(obj);
     
+    // Loop JSON object
     while(!json_object_iter_equal(&it, &itEnd)) {
         
-        // JSON에서 읽은 데이터 유형 얻기
-        type = json_type_to_name(json_object_get_type(json_object_iter_peek_value(&it)));
+        // Read data from iterator
+        cur = json_object_iter_peek_value(&it);
+        type = json_type_to_name(json_object_get_type(cur));
         name = json_object_iter_peek_name(&it);
         
+        // Input Data
         if(!strcmp(type,"string")) {
-            if(!strcmp("DB_HOST", name)) {
-                strcpy(cfg.HOST, json_object_get_string(json_object_iter_peek_value(&it)));
-            }
-            else if(!strcmp("DB_USER", name)) {
-                strcpy(cfg.USER, json_object_get_string(json_object_iter_peek_value(&it)));
-            }
-            else if(!strcmp("DB_PASS", name)) {
-                strcpy(cfg.PASS, json_object_get_string(json_object_iter_peek_value(&it)));
-            }
+
+            // DB host
+            if(!strcmp("DB_HOST", name))
+                strcpy(cfg.HOST, json_object_get_string(cur));
+
+            // DB user_id
+            else if(!strcmp("DB_USER", name))
+                strcpy(cfg.USER, json_object_get_string(cur));
+
+            // DB user_password
+            else if(!strcmp("DB_PASS", name))
+                strcpy(cfg.PASS, json_object_get_string(cur));
+
+            // ignore other type
             else
                 continue;        
         }
-        else if(!strcmp(type,"int")) {
-            cfg.PORT = json_object_get_int(json_object_iter_peek_value(&it));
-        }
-        else {
+
+        // DB port
+        else if(!strcmp(type,"int"))
+            cfg.PORT = json_object_get_int(cur);
+
+        // other type
+        else
             printf("Unkown Type.\n");
-        }
+
+        // Next iterator
         json_object_iter_next(&it);
     }
 }
